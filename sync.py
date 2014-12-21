34064d2550f19ff9aeed71b82ec6c374dd3dc22a
Kenneth Leftin <kjleftin@gmail.com>
2014-12-21 16:53:57 -0500

    Add command line arguments. Fetch tokens. Add some comments

--- sync.py
+++ sync.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
 from __future__ import unicode_literals
-
 import sys,os.path
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-import pprint
 import copy
+import argparse
 
 from python.rdio import Rdio
+
 from rdio_consumer_credentials import RDIO_CREDENTIALS
 from rdio_consumer_credentials import TOKEN
 try:
@@ -22,47 +21,47 @@ try:
 except NameError:
     pass
 
-pp = pprint.PrettyPrinter(depth=6)
+# Number of albums to fetch from Rdio in one request.
+PAGE_SIZE = 100
 
-    # create an instance of the Rdio object with our consumer credentials
-rdio = Rdio(RDIO_CREDENTIALS, TOKEN)
+# Number of albums to show on a single page in the interactive terminal.
+MAX_ALBUMS = 50
 
-# try:
-#   # authenticate against the Rdio service
-#   url = rdio.begin_authentication('oob')
-#   print('Go to: ' + url)
-#   verifier = input('Then enter the code: ').strip()
-#   rdio.complete_authentication(verifier)
+def main(argv):
+  """Runs the main program.
 
-#   print rdio.token
+  Intializes the Rdio connection and begins the prompt.
+  """
 
-#   # find out what playlists you created
-#   myPlaylists = rdio.call('getPlaylists')['result']['owned']
+  parser = argparse.ArgumentParser(description='Interactive command line interface for syncing music from Rdio.')
+  parser.add_argument('--fetch_token', action='store_true', default=False, help='Fetch a fresh OAuth token. If not provided, will expect TOKEN to be defined in rdio_consumer_credentials.py')
+  parser.add_argument('--fake', action='store_true', default=False, help='Use a fake set of albums. (For testing only)')
+  args = parser.parse_args()
+  if args.fetch_token:
+    rdio = fetchRdioWithToken()
+  else:
+    rdio = Rdio(RDIO_CREDENTIALS, TOKEN)
 
-#   # list them
-#   for playlist in myPlaylists:
-#     print('%(shortUrl)s\t%(name)s' % playlist)
-# except HTTPError as e:
-#   # if we have a protocol error, print it
-#   print(e.read())
+  if args.fake:
+    albums = fakeAlbums()
+  else:
+    albums = fetchAllAlbums(rdio)
 
+  sync = Sync(rdio, albums)
+  sync.prompt()
 
-# userid = rdio.call('findUser', {'email': 'kjleftin@gmail.com'})['result']['key']
-# print userid
-#pp.pprint(rdio.call('getOfflineTracks')['result']);
+def fetchRdioWithToken():
+  """Initialized Rdio with a token."""
 
-def fakeAlbums():
-  albums = [];
-  albums.append({'key': '1', 'name': 'Selling England by the Pound', 'artist': 'Genesis', 'offline': False})
-  albums.append({'key': '2', 'name': 'Foxtrot', 'artist': 'Genesis', 'offline': False})
-  albums.append({'key': '3', 'name': 'Word of Mouth', 'artist': 'The Kinks', 'offline': True})
-  albums.append({'key': '4', 'name': 'Wish You Were Here', 'artist': 'Pink Floyd', 'offline': False})
-  albums.append({'key': '5', 'name': 'Wishbone Ash', 'artist': 'Wishbone Ash', 'offline': True})
-  return albums
+  rdio = Rdio(RDIO_CREDENTIALS)
+  url = rdio.begin_authentication('oob')
+  print('Go to: ' + url)
+  verifier = input('Then enter the code: ').strip()
+  rdio.complete_authentication(verifier)
+  return rdio
 
-def fetchAllAlbums():
-  PAGE_SIZE = 100
-
+def fetchAllAlbums(rdio):
+  """Fetches albums for a user."""
   albums = []
   c = 0
   while 1:
@@ -71,7 +70,7 @@ def fetchAllAlbums():
     if len(resultList) > 0:
       for result in resultList:
         c += 1
-        if result['type'] != 'a':
+        if result['type'] != 'a' or not result['canTether'] or not result['canStream'] or not result['canSample']:
           continue
         result['offline'] = False
         albums.append(result)
@@ -95,8 +94,22 @@ def fetchAllAlbums():
 
   return albums
 
+# Album dict schema: http://www.rdio.com/developers/docs/web-service/types/album/ref-web-service-type-album
 class Sync:
-  MAX_ALBUMS = 50
+  """Class which holds state for syncing.
+
+  Holds a connection to Rdio and a mutable list of albums.
+
+  Attributes:
+    rdio: A connection to Rdio.
+    albums: A list of albums. The offline bit on each album may be mutated.
+    oldAlbums: A list of albums which should remain constant until after syncing.
+        This is used for diffing the mutated albums for syncing.
+    filterString: If set, will prompt() will only show albums or artists that
+        match this string.
+    offset: The page we are on. For example, '50' starts at the 50th element.
+    
+  """
 
   def __init__(self, rdio, albums):
     self.rdio = rdio
@@ -109,7 +122,7 @@ class Sync:
     filteredAlbums = self.albums
     if self.filterString != '':
       filteredAlbums = filter(lambda a: (self.filterString in a['name']) or (self.filterString in a['artist']), self.albums)
-    return filteredAlbums[self.offset:(Sync.MAX_ALBUMS + self.offset)]
+    return filteredAlbums[self.offset:(MAX_ALBUMS + self.offset)]
 
   def getTotalItemsWithFilter(self):
     filteredAlbums = self.albums
@@ -118,6 +131,8 @@ class Sync:
     return len(filteredAlbums)
 
   def prompt(self):
+    """Prompts the user to select from a list of albums"""
+
     selectChar = ' '
     for idx, album in enumerate(self.getAlbumView()):
       if album['offline']:
@@ -130,12 +145,14 @@ class Sync:
     len(filter(lambda album: album['offline'], self.albums))
     print 'Showing {0}-{1} of {2}. Albums Synced: {3}.'.format(
       str(self.offset),
-      str(min(self.getTotalItemsWithFilter(), self.offset + Sync.MAX_ALBUMS)),
+      str(min(self.getTotalItemsWithFilter(), self.offset + MAX_ALBUMS)),
       self.getTotalItemsWithFilter(),
       totalSynced)
     self.handleInput(raw_input('Toggle Album. Or [\'(a)ll\', \'(c)lear\', \'(n)ext\', \'(p)rev\', \'(f)ilter\', \'(s)ort\', \'sync\'] --> '))
 
   def handleInput(self, input):
+    """Handles the input from the prompt"""
+    
     if input == 'q':
       quit()
     elif input == 'sort' or input == 's':
@@ -157,11 +174,9 @@ class Sync:
     elif input.isdigit():
       # Toggle selected album.
       idx = int(input)
-      album = self.getAlbumView()[idx]
-      isOffline = False
-      if ('offline' in album) and album['offline']:
-        isOffline = True
-      album['offline'] = not isOffline
+      if idx < len(self.getAlbumView()):
+        album = self.getAlbumView()[idx]
+        album['offline'] = not album['offline']
     else:
       print "Unrecognized Input.\n"
     self.prompt()
@@ -173,12 +188,12 @@ class Sync:
     self.filterString = input
 
   def next(self):
-    self.offset += Sync.MAX_ALBUMS
+    self.offset += MAX_ALBUMS
     if self.offset > self.getTotalItemsWithFilter():
-      self.offset -= Sync.MAX_ALBUMS
+      self.offset -= MAX_ALBUMS
 
   def prev(self):
-    self.offset -= Sync.MAX_ALBUMS
+    self.offset -= MAX_ALBUMS
     if self.offset < 0:
       self.offset = 0
 
@@ -195,6 +210,8 @@ class Sync:
       print "Unrecognized Input.\n"
 
   def sync(self):
+    """Syncs to Rdio"""
+    
     toAdd = []
     toRemove = []
 
@@ -226,5 +243,16 @@ class Sync:
     self.oldAlbums = copy.deepcopy(self.albums)
     print "Done."
 
-sync = Sync(rdio, fetchAllAlbums())
-sync.prompt()
+def fakeAlbums():
+  """Provides a set of fake albums used for testing"""
+  
+  albums = [];
+  albums.append({'key': '1', 'name': 'Selling England by the Pound', 'artist': 'Genesis', 'offline': False})
+  albums.append({'key': '2', 'name': 'Foxtrot', 'artist': 'Genesis', 'offline': False})
+  albums.append({'key': '3', 'name': 'Word of Mouth', 'artist': 'The Kinks', 'offline': True})
+  albums.append({'key': '4', 'name': 'Wish You Were Here', 'artist': 'Pink Floyd', 'offline': False})
+  albums.append({'key': '5', 'name': 'Wishbone Ash', 'artist': 'Wishbone Ash', 'offline': True})
+  return albums
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
