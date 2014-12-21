8d1b126e376f6d7fe5b620186d239546973349ce
Kenneth Leftin <kjleftin@gmail.com>
2014-12-21 17:18:45 -0500

    Add copyright line

--- sync.py
+++ sync.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python
 
+# sync.py is an interactive command line interface for syncing music from Rdio.
+#
+# (c) 2014 Kenneth Leftin
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 from __future__ import unicode_literals
 import sys,os.path
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -245,7 +266,7 @@ class Sync:
 
 def fakeAlbums():
   """Provides a set of fake albums used for testing"""
-  
+
   albums = [];
   albums.append({'key': '1', 'name': 'Selling England by the Pound', 'artist': 'Genesis', 'offline': False})
   albums.append({'key': '2', 'name': 'Foxtrot', 'artist': 'Genesis', 'offline': False})
