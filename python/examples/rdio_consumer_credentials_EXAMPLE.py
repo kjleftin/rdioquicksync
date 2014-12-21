d23d820e62ed3df09c838467ce2214b8779ee460
Kenneth Leftin <kjleftin@gmail.com>
2014-12-21 17:13:01 -0500

    Remove unnecessary files

--- python/examples/rdio_consumer_credentials_EXAMPLE.py
+++
@@ -1,9 +0,0 @@
-# Copy this file to rdio_consumer_credentials.py and add your
-# OAuth keys to get the examples to work.
-#
-# You can get OAuth keys by signing up for a developer account at:
-# http://www.rdio.com/developers/
-RDIO_CONSUMER_KEY = 'your key here'
-RDIO_CONSUMER_SECRET = 'your secret here'
-
-RDIO_CREDENTIALS = (RDIO_CONSUMER_KEY, RDIO_CONSUMER_SECRET)
